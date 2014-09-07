77b0d05f44c6fe8f9c406658d644699e1281e7be
Joshua D. Katz <gravypod@gravypod.com>
2014-09-07 22:37:36 +0100

    First commit of our event stack

---
+++ src/org/starmod/eventstack/Event.java
@@ -0,0 +1,27 @@
+package org.starmod.eventstack;
+
+/**
+ * An event object is passed to {@link EventHandler}s whenever a corresponding action takes place
+ *
+ */
+public abstract class Event {
+
+	private boolean canceled = false;
+
+	/**
+	 * Has the event been canceled?
+	 * @return True if it has
+	 */
+	public boolean isCanceled() {
+
+		return canceled;
+	}
+
+	/**
+	 * Cancel this event
+	 */
+	public void cancel() { 
+		canceled = true; // Prevent people from re-activating an event 
+	}
+
+}
---
+++ src/org/starmod/eventstack/EventHandler.java
@@ -0,0 +1,37 @@
+package org.starmod.eventstack;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Allow methods to handle events
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface EventHandler {
+
+	/**
+	 * Handle events that have been canceled already.
+	 * True means that this event handler WILL recive them
+	 */
+	boolean onCanceled() default true;
+
+	/**
+	 * What priority of execution should this event handler have?
+	 *
+	 * <pre>
+	 * MONITOR - Do not cancel on monitor. This handler is for OBSERVATIONS only
+	 * LOW - Happens before MONITOR. Use this if you do not plan to cancel events.
+	 * NORMAL - Happens at a normal time after HIGH and before LOW
+	 * HIGH - Happens first
+	 * </pre>
+	 *
+	 *
+	 *
+	 */
+	EventPriority priority() default EventPriority.NORMAL;
+
+}
